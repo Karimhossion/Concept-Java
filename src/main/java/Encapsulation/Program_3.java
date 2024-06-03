be5792bb25ba774cf6538c9bdd1ce1ecbf39fd15
Karim Hossion <118505582+Karimhossion@users.noreply.github.com>
2024-06-03 08:03:06 +0600

    Update Program_3.java

--- src/main/java/Encapsulation/Program_3.java
+++ src/main/java/Encapsulation/Program_3.java
@@ -44,3 +44,60 @@ public class Program_3 {
         System.out.println("WIdth : "+r.getwidth());
     }
 }
+
+
+
+/*
+import java.util.Scanner;
+
+class Rectangle {
+    private double length;
+    private double width;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    // Method to calculate area
+    public double calculateArea() {
+        return length * width;
+    }
+
+    // Method to calculate perimeter
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+public class Program_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter length: ");
+        double length = input.nextDouble();
+        System.out.println("Enter width: ");
+        double width = input.nextDouble();
+        
+        Rectangle r = new Rectangle();
+        r.setLength(length);
+        r.setWidth(width);
+        
+        System.out.println("Length: " + r.getLength());
+        System.out.println("Width: " + r.getWidth());
+        System.out.println("Area: " + r.calculateArea());
+        System.out.println("Perimeter: " + r.calculatePerimeter());
+    }
+}
+*/
